99b6b5526e42e7467eea1d6f7c20407d98f4eae3
thecodeofyamid <yhrodriguez1@hotmail.com>
2024-12-28 20:19:27 -0500

    integración con tailwindcss

---
+++ app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/**/*.{astro,html,js,jsx,ts,tsx}',
+      './public/**/*.html',
+      './public/**/*.js',
+      './public/**/*.css',
+      '*.html',
+      '*.astro',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
